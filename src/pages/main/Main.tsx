7af8e5a28c6bb359c402d538ccf901f06ffa0408
se0jinYoon <tjwls1999@gmail.com>
2024-07-29 03:52:02 +0900

    chore: TestDiv 생성

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,11 +1,19 @@
+import styled from '@emotion/styled';
+
 import { HbPromiseIc } from '../../assets/svgs';
 
 const Main = () => {
   return (
     <>
       <HbPromiseIc />
+      <TestDiv>안녕하세요</TestDiv>
     </>
   );
 };
 
 export default Main;
+
+const TestDiv = styled.div`
+  color: ${({ theme }) => theme.colors.Blue};
+  ${({ theme }) => theme.fonts.Head1_B_20};
+`;
