e4358ed12ae3b5ceb7d6d7d1c5cf81cb7a36c947
se0jinYoon <tjwls1999@gmail.com>
2024-07-29 02:20:34 +0900

    setting: 폴더구조 세팅

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import Main from './pages/main/Main';
+
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>{/* <Route path="/" element={<Main />} /> */}</Routes>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
     </BrowserRouter>
   );
 };
