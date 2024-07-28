f94cdc8f4d69d85cbb963a962e018ea0d9174d1f
se0jinYoon <tjwls1999@gmail.com>
2024-07-29 03:40:33 +0900

    design: 모바일뷰 세팅 변경

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ export default App;
 
 const Wrapper = styled.div`
   width: 100vw;
-  min-height: calc(var(--vh, 1vh) * 100 - 44px);
+  min-height: calc(var(--vh, 1vh) * 100);
   border: none;
 
   background-color: #fff;
