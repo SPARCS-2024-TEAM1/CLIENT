ec8ac2c58c69eedc021bf282eb5ea32656fb6d08
se0jinYoon <tjwls1999@gmail.com>
2024-07-29 19:45:43 +0900

    feat: 하단 fullBtn 기능 구현

---
+++ src/components/commons/FullBtn.tsx
@@ -0,0 +1,56 @@
+import styled from '@emotion/styled';
+
+interface fullBtnPropsType {
+  activeText?: string;
+  disabledText?: string;
+  isBtnDisable: boolean;
+  onClick?: () => void;
+}
+
+const FullBtn = (props: fullBtnPropsType) => {
+  const { activeText, disabledText, isBtnDisable, onClick } = props;
+  return (
+    <Wrapper>
+      <Button $isBtnDisable={isBtnDisable} onClick={onClick}>
+        {isBtnDisable ? disabledText : activeText}
+      </Button>
+      <BtnBackground />
+    </Wrapper>
+  );
+};
+
+export default FullBtn;
+
+const Wrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+
+  width: 100%;
+  margin-left: -2rem;
+  padding: 0 2rem;
+`;
+
+const Button = styled.button<{ $isBtnDisable: boolean }>`
+  z-index: 2;
+
+  width: 100%;
+  height: 5.6rem;
+  padding: 0 1.5rem;
+  border: none;
+  border-radius: 5px;
+
+  background-color: ${({ $isBtnDisable, theme }) => ($isBtnDisable ? theme.colors.grayScaleMg : theme.colors.key)};
+
+  /* 폰트 색상 변경 필요 */
+  color: ${({ $isBtnDisable, theme }) => ($isBtnDisable ? theme.colors.transparentW80 : theme.colors.grayScaleBg)};
+  ${({ theme }) => theme.fonts.Body2_SB_18};
+
+  cursor: ${({ $isBtnDisable }) => ($isBtnDisable ? 'default' : 'cursor')};
+`;
+
+const BtnBackground = styled.div`
+  width: 100%;
+  height: 2.8rem;
+
+  background-color: ${({ theme }) => theme.colors.grayScaleBg};
+`;
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,19 +1,32 @@
-import styled from '@emotion/styled';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { HbPromiseIc } from '../../assets/svgs';
+import FullBtn from '../../components/commons/FullBtn';
 import Input from '../../components/commons/Input';
 
 const Main = () => {
+  const navigate = useNavigate();
   const [inputVal, setInputVal] = useState('');
 
   const handleInputVal = (val: string) => {
     setInputVal(val);
   };
+
+  const handleOnClickFullBtn = () => {
+    navigate('/');
+  };
+
   return (
     <>
       <HbPromiseIc />
       <Input placeholder="닉네임을 입력해주세요" inputVal={inputVal} handleInputVal={handleInputVal} wordLimit={20} />
+      <FullBtn
+        disabledText="닉네임을 입력해주세요"
+        activeText="감정 기록해볼까요?"
+        isBtnDisable={inputVal.length > 20 || inputVal.length === 0}
+        onClick={handleOnClickFullBtn}
+      />
     </>
   );
 };
