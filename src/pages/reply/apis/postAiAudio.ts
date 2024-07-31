367a140cb4618fd59a716855ce23e3a75486d63e
se0jinYoon <tjwls1999@gmail.com>
2024-08-01 06:33:46 +0900

    chore: 주석 제거

--- src/pages/reply/apis/postAiAudio.ts
+++ src/pages/reply/apis/postAiAudio.ts
@@ -1,27 +1,13 @@
 // ai 음성 파일 post
 
-import axios from 'axios';
-
 import { client } from '../../../utils/apis/axios';
-
-// export const postAiAudio = async (moodDiaryId: number) => {
-//   try {
-//     const response = await client.post(`/api/v1/audio`, { moodDiaryId });
-
-//     console.log(response);
-//     return response.blob();
-//   } catch (err) {
-//     console.error('ai 음성 파일 post 에러: ', err);
-//     throw err;
-//   }
-// };
 
 export const postAiAudio = async (moodDiaryId: number) => {
   try {
     const response = await client.post(`/api/v1/audio`, { moodDiaryId }, { responseType: 'blob' });
 
-    console.log(response.data);
-    return response.data; // response.data가 Blob 타입으로 반환됩니다.
+    // console.log(response.data);
+    return response.data; // Blob 타입으로 반환
   } catch (err) {
     console.error('ai 음성 파일 post 에러: ', err);
     throw err;
